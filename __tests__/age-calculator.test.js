7a93eea4e09819f422248a83ae74f51f9b4bef80
Jesse White <jesse.white6@gmail.com>
2021-04-16 08:31:42 -0700

    pass failing test for User creation.

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -2,7 +2,7 @@ import User from './../src/js/age-calculator.js'
 
 describe('User', () => {
     test('it should create a new User object with empty fields', () => {
-        let newUser = new User();
-        expect(newUser.name).toEqual("undefined");
+        let newUser = new User("Jesse");
+        expect(newUser.name).toEqual("Jesse");
     });
 });
--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -1,5 +1,7 @@
 export default class User {
     constructor(name, currentAge, residentPlanet) {
-
+        this.name = name;
+        this.currentAge = currentAge;
+        this.residentPlanet = residentPlanet;
     }
 }
