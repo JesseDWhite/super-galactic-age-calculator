8ab11d9388756230d418ed6269ee186a12b12777
Jesse White <jesse.white6@gmail.com>
2021-04-16 08:51:49 -0700

    refactor User class to include all planets.

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -2,8 +2,13 @@ export default class User {
     constructor(name, currentAge, residentPlanet) {
         this.name = name;
         this.currentAge = currentAge;
-        this.residentPlanet = residentPlanet;
+        this.residentPlanet = "Earth";
         this.convertedAge = 0;
+        this.mercuryAge = 0;
+        this.venusAge = 0;
+        this.marsAge = 0;
+        this.jupiterAge = 0;
     }
-
+    convertAge() {
+    }
 }
