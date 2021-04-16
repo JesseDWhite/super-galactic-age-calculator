30bc6110a2bc62deb58c9c4699c8dbf73e42a403
Jesse White <jesse.white6@gmail.com>
2021-04-16 08:37:21 -0700

    refactor previous passing test.

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -1,8 +1,10 @@
 import User from './../src/js/age-calculator.js'
 
 describe('User', () => {
-    test('it should create a new User object with empty fields', () => {
-        let newUser = new User("Jesse");
+    test('it should show the users name, age, and current planet after construction.', () => {
+        let newUser = new User("Jesse", 27, "Earth")
         expect(newUser.name).toEqual("Jesse");
+        expect(newUser.currentAge).toEqual(27);
+        expect(newUser.residentPlanet).toEqual("Earth");
     });
 });
